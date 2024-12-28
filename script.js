6a9638e48069611f5e71e4591203f3d9e978f8c9
Rohan0004 <110601451+Rohan0004@users.noreply.github.com>
2024-12-28 18:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,11 @@ function updURL() {
 	var name = document.getElementById("name").value;
 	var year = document.getElementById("year").value;
 	var url = "https://localhost:8080/";
-	if (name && year) {
-        // Concatenate both name and year to the URL
+	if (name && year) {       
         url += "?name=" + name + "&year=" + year;
     } else if (name) {
-        // Concatenate only name to the URL
         url += "?name=" + name;
     } else if (year) {
-        // Concatenate only year to the URL
         url += "?year=" + year;
     }
 	document.getElementById("url").textContent = url;
