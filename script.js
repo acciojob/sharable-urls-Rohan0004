deaf862c984ea0650a3ea03e9f4f6e0151001e6e
Rohan0004 <110601451+Rohan0004@users.noreply.github.com>
2024-12-28 18:37:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// your code here
+function updURL() {
+	 event.preventDefault();
+	var name = document.getElementById("name").value;
+	var year = document.getElementById("year").value;
+	var url = "https://localhost:8080/";
+	if (name && year) {
+        // Concatenate both name and year to the URL
+        url += "?name=" + name + "&year=" + year;
+    } else if (name) {
+        // Concatenate only name to the URL
+        url += "?name=" + name;
+    } else if (year) {
+        // Concatenate only year to the URL
+        url += "?year=" + year;
+    }
+	document.getElementById("url").textContent = url;
+}
